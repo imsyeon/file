83c09412705f71564e1554bf51b1013951c83bbb
sue <sooyeonlim0412@gmail.com>
2022-02-21 18:41:09 +0900

    Feat: create uploadFile method in FileController it's not completed

--- src/main/java/com/example/suefile/controller/FileController.java
+++ src/main/java/com/example/suefile/controller/FileController.java
@@ -1,16 +1,13 @@
 package com.example.suefile.controller;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.net.URI;
@@ -52,5 +49,41 @@ public class FileController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @PutMapping("/upload")
+    public void uploadFile(@RequestParam ("file") MultipartFile multipartFile) throws IOException {
+
+        String username = "username";
+        String password = "password";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth(username, password);
+
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        System.out.println(headers);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", multipartFile.getResource());
+
+        System.out.println("byte"+multipartFile.getBytes());
+        System.out.println("리소스"+multipartFile.getResource());
+        System.out.println("이름"+multipartFile.getName());
+        System.out.println("사이즈"+multipartFile.getSize());
+        System.out.println("body"+body);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        String url = "http://49.247.36.30:8090/revit/remote.php/dav/files/sooyeon/testfolder/"+multipartFile.getOriginalFilename();
+
+        System.out.println("responseEntity"+requestEntity.getBody());
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate
+                .exchange(url, HttpMethod.PUT, requestEntity, String.class);
+
+        System.out.println(response.getStatusCode());
+
     }
 }
